159a60318dff6ce66e9dae39a47d28c8d96e5415
Benjamin Bennett <bbennett@redhat.com>
2018-10-18 15:32:07 -0400

    Made it ignore the state files (clockisticking and showsource)

--- utils/update.sh
+++ utils/update.sh
@@ -13,18 +13,19 @@
 RSYNC_SOURCE="fiji@limey.net:kindle/kindle-clock"
 
 # The target filesystem doesn't allow modification of mtime, setting of user, or setting of permissions
-RSYNC_OPTIONS="-rltD --omit-dir-times"
+RSYNC_OPTIONS="-vi -rltD --omit-dir-times"
 
 cd /mnt/us
 
 # Update all of the files in util and completely replace them with the new stuff
 # To do this manually:
-#  /usr/bin/rsync -rltD --omit-dir-times -delete-delay --delay-updates fiji@limey.net:kindle/kindle-clock/utils/ utils
+#  /usr/bin/rsync -rltD --omit-dir-times --delete-delay --delay-updates fiji@limey.net:kindle/kindle-clock/utils/ utils
 /usr/bin/rsync $RSYNC_OPTIONS --delete-delay --delay-updates "${RSYNC_SOURCE}/utils/" utils
 
 # Update all of the files in timelit and completely replace them with the new stuff
 # BUT ignore the images, we do that later
-/usr/bin/rsync $RSYNC_OPTIONS --delete --exclude /images "${RSYNC_SOURCE}/timelit/" timelit
+# AND do not update the showsource and clockisticking state files
+/usr/bin/rsync $RSYNC_OPTIONS --delete --exclude /images --exclude /showsource --exclude /clockisticking "${RSYNC_SOURCE}/timelit/" timelit
 
 # Update the images, but we don't care about the times
 /usr/bin/rsync $RSYNC_OPTIONS --size-only --delete "${RSYNC_SOURCE}/timelit/images/" timelit/images
