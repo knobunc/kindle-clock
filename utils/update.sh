5e49d3a8edf4824508dbde49017f25170fb2870e
Benjamin Bennett <bbennett@redhat.com>
2019-01-04 11:29:29 -0500

    Made the status messages cleaner for updates

--- utils/update.sh
+++ utils/update.sh
@@ -28,26 +28,26 @@ cd /mnt/us
 # Update all of the files in util and completely replace them with the new stuff
 # To do this manually:
 #  /usr/bin/rsync -rltD --omit-dir-times --delete-delay --delay-updates fiji@limey.net:kindle/kindle-clock/utils/ utils
-add_status "Updating utils..."
+add_status "Updating utils...        "
 /usr/bin/rsync $RSYNC_OPTIONS --delete-delay --delay-updates "${RSYNC_SOURCE}/utils/" utils
 
 # Update all of the files in timelit and completely replace them with the new stuff
 # BUT ignore the images, we do that later
 # AND do not update the showsource and clockisticking state files
-add_status "Updating timelit..."
+add_status "Updating timelit...      "
 /usr/bin/rsync $RSYNC_OPTIONS --delete-delay --exclude /images --exclude /showsource --exclude /clockisticking "${RSYNC_SOURCE}/timelit/" timelit
 
 # Update the images, but we don't care about the times
-add_status "Updating images..."
+add_status "Updating images...       "
 /usr/bin/rsync $RSYNC_OPTIONS --size-only --delete-delay "${RSYNC_SOURCE}/timelit/images/" timelit/images
 
 # Update the launchpad init file, but leave the rest of the directory alone
-add_status "Updating launchpad..."
+add_status "Updating launchpad...    "
 /usr/bin/rsync $RSYNC_OPTIONS "${RSYNC_SOURCE}/launchpad/" launchpad
 
 # Restart launchpad so the command changes take effect
 /etc/init.d/launchpad restart
 
-add_status "Update complete!"
+add_status "Update complete!         "
 rm updatestatus
 
