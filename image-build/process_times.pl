f4d2e6d8b0918f20b238c36efaaeadeecb71a518
Benjamin Bennett <bbennett@redhat.com>
2019-01-22 15:16:48 -0500

    Missed one

--- image-build/process_times.pl
+++ image-build/process_times.pl
@@ -8,16 +8,13 @@ use open ':std', ':encoding(UTF-8)';
 # Force the XS parsers
 BEGIN {
     $ENV{PERL_TEXT_CSV}     = 'Text::CSV_XS';
-    $ENV{PERL_JSON_BACKEND} = 'JSON::XS';
 }
 
-use File::Slurper qw( read_binary );
-use JSON;
 use Text::CSV;
-use XML::Entities;
 
 use lib '.';
 
+use Process;
 use TimeMatch;
 
 my $DEBUG = 0;
@@ -42,124 +39,11 @@ sub main {
     die "Usage: $0 idir"
         unless defined $idir and -d $idir;
 
-    my $awards = load_awards();
     my $times  = load_times($idir);
 
-    my @types = qw( <<  <  ~  -  +  >  >> );
-
-    foreach my $hr (0 .. 23) {
-        foreach my $mn (0 .. 59) {
-            my $t = sprintf("%02d:%02d", $hr, $mn);
-            my $ta = $times->{$t} // [];
-
-            my $w  = 0;
-            my $x  = 0;
-            my $ap = 0;
-            my %tc;
-            foreach my $e (@$ta) {
-                my ($a, $r, undef, undef, $title, $author) = @$e;
-
-                $w++ if $awards->{$author}{$title};
-
-                if (not $a and not $r) {
-                    $x++;
-                    next;
-                }
-                $ap++ if $a;
-                if ($r) {
-                    $tc{$r}++;
-                }
-            }
-
-            my $rs = join("  ", map({sprintf("%4d%s", $tc{$_}//0, $_)} @types));
-
-            # Time: Total AMPM Exact Awards Relatives
-            printf(">> %5s: %4dT  %4dA  %4dE  %4d*  %s\n",
-                   $t, int(@{ $times->{$t} || [] }), $ap, $x, $w, $rs);
-
-            my $sorted = sort_matches($ta, $awards);
-            foreach my $i (0..9) {
-                last if $i >= @$sorted;
-                my ($a, $r, $timestr, $quote, $title, $author) = @{ $sorted->[$i] };
-                $timestr =~ s{[\n\t]}{ }g;
-                $timestr =~ s{\s\s+}{ }g;
-                $timestr =~ s{^\s+}{};
-                $timestr =~ s{\s+$}{};
-
-                my $award = $awards->{$author}{$title};
-                die "No awards entry for '$author' '$title'"
-                    unless defined $award;
-
-                my $w = $award ? "*" : '';
-                $r //= defined $a ? ''  : 'E';
-                $a   = defined $a ? 'A' : '';
-                printf("      %1s%1s%2s %-37.37s -- %20.20s | %-20.20s\n",
-                       $w, $a, $r, '"'.$timestr.'"', $author, $title);
-            }
-            print "            ..."
-                if @$sorted > 10;
-            print "\n";
-        }
-    }
+    print_matches($times);
 
     return 0;
-}
-
-sub sort_matches {
-    my ($matches, $awards) = @_;
-
-    my %rel_to_dist =
-        (
-         'E'  =>   0,
-         '<<' => -20,
-         '<'  => -10,
-         '~'  => -20,
-         '-'  => -10,
-         '+'  => -20,
-         '>'  => -10,
-         '>>' => -20,
-        );
-
-    # Preprocess the rows
-    my @processed;
-    foreach my $m (@$matches) {
-        my ($ampm, $rel, $timestr, $quote, $title, $author) = @$m;
-        my $exact = (not $ampm and not $rel) ? 1 : 0;
-        my $award = $awards->{$author}{$title} // 0;
-
-        my $distance = $rel_to_dist{$rel // 'E'}
-            // die "No distance for '$rel'";
-
-        $distance -= 5 if defined $ampm;
-
-        push @processed, [$exact, $award, $distance, $m];
-    }
-
-    my @sorted =
-        (sort {
-            # In order of preference:
-            return
-                #  - Exact time
-                $b->[0] <=> $a->[0] ||
-
-                #  - Distance
-                $b->[2] <=> $a->[2] ||
-
-                #  - Num awards
-                $b->[1] <=> $a->[1] ||
-
-                # The pointers themselves as a fallback
-                $a cmp $b;
-         }
-         @processed);
-    if (@processed > 5 and @processed < 10 and 0) {
-        my @p = map { [ @{$_}[0..2], @{ $_->[3] }[0..2] ] } @processed;
-        my @s = map { [ @{$_}[0..2], @{ $_->[3] }[0..2] ] } @sorted;
-        DEBUG_MSG(\@p, \@s), exit;
-    }
-
-
-    return [ map { $_->[3] } @sorted ];
 }
 
 sub load_times {
@@ -215,37 +99,4 @@ sub load_times {
     closedir $idh;
 
     return \%times;
-}
-
-# Gets a hashref with author -> book -> num_awards
-sub load_awards {
-    # No args
-
-    my %awards;
-
-    my @files = glob("$ENV{HOME}/Calibre\\\ Library/*/*/metadata.opf")
-	or die "Unable to find metadata files";
-    foreach my $f (@files) {
-        $f =~ m{\A \Q$ENV{HOME}/Calibre Library\E /
-                   (?<author> .+?) /
-                   (?<book> .+?) \s \(\d+\) /
-                   metadata\.opf
-                \z}xn
-            or die "Bad metadata file '$f'";
-        my ($author, $book) = @+{qw{ author book }};
-        $book =~ s{_ .*}{};
-
-        die "No file '$f'" unless -f $f;
-        my $c = read_binary($f);
-
-        my ($content) = $c =~ m{ <meta \s+ name="calibre:user_metadata:\#awards"
-                                \s+ content="([^"]+)" />
-                               }x;
-        $content = decode_json( XML::Entities::decode('all', $content) );
-        my $awards = $content->{'#value#'};
-
-        $awards{$author}{$book} = @$awards;
-    }
-
-    return \%awards;
 }
